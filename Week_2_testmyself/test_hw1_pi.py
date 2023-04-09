70ac4767af5fd977de4c96626f6e8a1a9abe21f7
Hythloday1907 <110468099+Hythloday1907@users.noreply.github.com>
2023-04-09 14:38:36 +0300

    test
    test

---
+++ Week_2_testmyself/test_hw1_pi.py
@@ -0,0 +1,17 @@
+import pytest
+from hw1_pi import test_major_and_minor_elem
+
+LISTS = [
+    ([2, 2, 1, 1, 1, 2, 2], (2, 1)), 
+    ([3, 2, 3], (3, 2)),
+    ([3, 3, 2, 2, 3, 3, 1], (3, 1))
+]
+
+@pytest.mark.parametrize(
+    "numbers, expected", LISTS
+)
+def test_major_and_minor_elem(numbers, expected):
+
+    assert test_major_and_minor_elem(numbers) == expected
+
+
